d7ab45efd722e54a3b5dbf9cf6284678b108cb2f
paskuvan <majopaskuvan@gmail.com>
2019-04-22 22:03:35 -0300

    ex01 ok

---
+++ ex01.js
@@ -0,0 +1,5 @@
+var a = parseInt(prompt("ingresa un número"));
+var b = parseInt(prompt("ingresa otro número"));
+var total = a + b
+alert("La suma total es " + total );
+alert("La suma total es " + (a + b));
